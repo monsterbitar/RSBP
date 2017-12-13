a88cf2a7dcd2155022e51dbfe84f5baf2a1d6469
Jonathan Silverblood <github@monsterbitar.se>
2017-12-13 10:20:47 +0100

    Simplify translation structure.

--- app/translation.js
+++ app/translation.js
@@ -1,54 +1,42 @@
 let SPOS_LANGUAGE = { // eslint-disable-line no-unused-vars
-  "words": {
-    "payment_modal_amount": {
-      "en": "Amount",
-	 "sv": "Belopp"
-    },
-    "payment_modal_discount": {
-      "en": "Discount",
-	 "sv": "Rabatt"
-    },
-    "payment_modal_total_currency": {
-      "en": "Total",
-	 "sv": "Belopp"
-    },
-    "payment_modal_rate": {
-      "en": "Exchange rate",
-	 "sv": "Växlingskurs"
-    },
-    "pay_button": {
-      "en": "Pay",
-	 "sv": "Betala"
-    },
-    "temp2": {
-      "en": "Amount",
-	 "sv": "Belopp"
-    },
-    "temp1": {
-      "en": "Total",
-	 "sv": "Summa"
-    }
+  "payment_modal_amount": {
+    "en": "Amount",
+    "sv": "Belopp"
   },
-  "phrases": {
-    "meta_title": {
-      "en": "Simple Point of Sale for Bitcoin Cash",
-	 "sv": "Enkel Kassa för Bitcoin Cash"
-    },
-    "payment_status_tx_details": {
-      "en": "See transaction details",
-	 "sv": ""
-    },
-    "order_title": {
-      "en": "Payment Request",
-	 "sv": "Betalförfrågan"
-    },
-    "meta_title2": {
-      "en": "Simple Point of Sale for Bitcoin Cash",
-	 "sv": "Enkel Kassa för Bitcoin Cash"
-    },
-    "meta_title2": {
-      "en": "Simple Point of Sale for Bitcoin Cash",
-	 "sv": "Enkel Kassa för Bitcoin Cash"
-    },
+  "payment_modal_discount": {
+    "en": "Discount",
+    "sv": "Rabatt"
+  },
+  "payment_modal_total_currency": {
+    "en": "Total",
+    "sv": "Summa"
+  },
+  "payment_modal_rate": {
+    "en": "Exchange rate",
+    "sv": "Växlingskurs"
+  },
+  "payment_modal_total_payment": {
+    "en": "Pay",
+    "sv": "Betala"
+  },
+  "pay_button": {
+    "en": "Pay",
+    "sv": "Betala"
+  },
+  "meta_title": {
+    "en": "Simple Point of Sale for Bitcoin Cash",
+    "sv": "Enkel Kassa för Bitcoin Cash"
+  },
+  "payment_status_tx_details": {
+    "en": "See transaction details",
+    "sv": ""
+  },
+  "order_title": {
+    "en": "Payment Request",
+    "sv": "Betalförfrågan"
+  },
+  "...": {
+    "en": "...",
+    "sv": "..."
   }
 };
