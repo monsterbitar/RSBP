461ade4e83bffe1211e1cef1f980fab8e33f9571
Jonathan Silverblood <github@monsterbitar.se>
2017-12-13 09:35:02 +0100

    Initial translation patch, used to test the translation structure.

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@
 /* global $ */
 /* global RSBP_CONFIG */
 /* global RSBP */
+/* global SPOS_LANGUAGE */
 
 (function () {
   $(document).ready(function() {
@@ -25,6 +26,19 @@
 
     // Setup currency amount input field
     $("#currency-amount-input-field").val(AMOUNT);
+
+    // Set up langague strings.
+    $("#payment-modal-amount-text").val(SPOS_LANGUAGE.words.payment_modal_amount);
+    $("#payment-modal-discount-text").val(SPOS_LANGUAGE.words.payment_modal_discount);
+    $("#payment-modal-total-currency-text").val(SPOS_LANGUAGE.words.payment_modal_total_currency);
+    $("#payment-modal-rate-text").val(SPOS_LANGUAGE.words.payment_modal_rate);
+    $("#meta-title-text").val(SPOS_LANGUAGE.phrases.meta_title);
+    $("#payment-status-tx-details-text").val(SPOS_LANGUAGE.phrases.payment_status_tx_details);
+    $("#order-title-text").val(SPOS_LANGUAGE.phrases.order_title);
+
+	// Set up language on the payment button
+    $("#pay-button").attr("value", SPOS_LANGUAGE.phrases.meta_title);
+    
 
     // Log payment modal state
     $("#payment-modal").on("shown.bs.modal", function () {
---
+++ app/translation.js
@@ -0,0 +1,57 @@
+// payment-modal-total-value-bch > payment-modal-total-payment-value
+// payment-modal-total-value-currency > payment-modal-total-currency-text
+
+let SPOS_LANGUAGE = { // eslint-disable-line no-unused-vars
+  "words": {
+    "payment_modal_amount": {
+      "en": "Amount",
+	 "sv": "Belopp"
+    },
+    "payment_modal_discount": {
+      "en": "Discount",
+	 "sv": "Rabatt"
+    },
+    "payment_modal_total_currency": {
+      "en": "Amount",
+	 "sv": "Belopp"
+    },
+    "payment_modal_rate": {
+      "en": "Exchange rate",
+	 "sv": "Växlingskurs"
+    },
+    "temp": {
+      "en": "Amount",
+	 "sv": "Belopp"
+    },
+    "temp2": {
+      "en": "Amount",
+	 "sv": "Belopp"
+    },
+    "temp1": {
+      "en": "Total",
+	 "sv": "Summa"
+    }
+  },
+  "phrases": {
+    "meta_title": {
+      "en": "Simple Point of Sale for Bitcoin Cash",
+	 "sv": "Enkel Kassa för Bitcoin Cash"
+    },
+    "payment_status_tx_details": {
+      "en": "See transaction details",
+	 "sv": ""
+    },
+    "order_title": {
+      "en": "Payment Request",
+	 "sv": "Betalförfrågan"
+    },
+    "meta_title": {
+      "en": "Simple Point of Sale for Bitcoin Cash",
+	 "sv": "Enkel Kassa för Bitcoin Cash"
+    },
+    "meta_title": {
+      "en": "Simple Point of Sale for Bitcoin Cash",
+	 "sv": "Enkel Kassa för Bitcoin Cash"
+    },
+  }
+};
